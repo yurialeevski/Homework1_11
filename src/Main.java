49fc360509d9273e81ea4747903125854ac48f4b
ya121 <ya7771@yandex.ru>
2022-12-29 23:13:44 +0300

    Доработано ДЗ № 1_11 все три замечания от 29 декабря

--- src/Main.java
+++ src/Main.java
@@ -8,58 +8,53 @@ public class Main {
     }
     public static void task1() {
         System.out.println("Задача 1");
-        identifyLeap_Year(2022);
+        identifyLeap_Year(2024);
     }
     public static void task2() {
         System.out.println("Задача 2");
-        byte clientOS = 0;
+        byte clientOS = 1;
         int clientDeviceYear = 2021;
         installApp(clientOS, clientDeviceYear);
     }
     public static void task3() {
         System.out.println("Задача 3");
-        int distance = 21;
+        int distance = 102;
         deliverInDays(distance);
     }
+
     public static void identifyLeap_Year(int year) {
-        if(year % 100 != 0) {
-            if (year % 4 == 0) {
-                System.out.println(year + " год - високосный год");
-            } else {
-                System.out.println(year + " год - невисокосный год");
-            }
-        } else if(year % 400 == 0) {
-            System.out.println(year + " год - високосный год");
-        } else {
+        if((year % 100 == 0 && year % 400 != 0) || year % 4 != 0) {
             System.out.println(year + " год - невисокосный год");
+        } else {
+            System.out.println(year + " год - високосный год");
         }
     }
 
     public static void installApp(byte clientOS, int clientDeviceYear ) {
-        int currentYear = LocalDate.now().getYear();
-        //System.out.println(currentYear);
-        if(clientOS == 0) {
-            if(clientDeviceYear < currentYear) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            }
-        }
-        if(clientOS == 1) {
-            if(clientDeviceYear < currentYear) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для Android по ссылке");
-            }
-        }
         if(clientOS !=0 && clientOS !=1) {
             System.out.println("Введен неправильный номер " + clientOS + ". Введите правильный номер: 0 — iOS, 1 — Android");
+            return;
         }
+        int currentYear = LocalDate.now().getYear();
+        if(clientDeviceYear < currentYear) {
+            System.out.print("Установите облегченную версию приложения для ");
+        } else {
+            System.out.print("Установите версию приложения для ");
+        }
+        if(clientOS == 0) {
+            System.out.println("iOS по ссылке");
+        } else {
+                System.out.println("Android по ссылке");
+            }
     }
 
     public static void deliverInDays(int deliveryDistance) {
+        if(deliveryDistance <= 0) {
+            System.out.println("Расстояние введено неправильно:" + deliveryDistance);
+            return;
+        }
         int daysToDeliver;
-        if(deliveryDistance >= 0 && deliveryDistance < 20) {
+        if(deliveryDistance > 0 && deliveryDistance < 20) {
             daysToDeliver = 1;
             System.out.println("Потребуется дней: " + daysToDeliver);
         }
@@ -73,9 +68,6 @@ public class Main {
         }
         if(deliveryDistance > 100) {
             System.out.println("Расстояние " + deliveryDistance + " км. На расстояние больше 100 км доставки нет");
-        }
-        if(deliveryDistance < 0) {
-            System.out.println("Расстояние введено неправильно:" + deliveryDistance);
         }
     }
 }
